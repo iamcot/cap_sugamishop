d0043dd542d52f6c45fe8e806f0acd276ff7e880
CoT <iamcot@gmail.com>
2025-03-29 22:50:16 +0700

    implement afterReadItem handler to filter items with stock > 0

--- srv/src/main/java/vn/sugami/sugamishop/handlers/ItemServiceHandler.java
+++ srv/src/main/java/vn/sugami/sugamishop/handlers/ItemServiceHandler.java
@@ -1,10 +1,15 @@
 package vn.sugami.sugamishop.handlers;
 
+import java.util.ArrayList;
+import java.util.stream.Collectors;
+
 import org.springframework.stereotype.Component;
 
 import com.sap.cds.services.EventContext;
+import com.sap.cds.services.cds.CdsReadEventContext;
 import com.sap.cds.services.cds.CqnService;
 import com.sap.cds.services.handler.EventHandler;
+import com.sap.cds.services.handler.annotations.After;
 import com.sap.cds.services.handler.annotations.On;
 import com.sap.cds.services.handler.annotations.ServiceName;
 
@@ -19,5 +24,17 @@ public class ItemServiceHandler implements EventHandler {
         System.out.println("ItemServiceHandler.onReadItem() called");
 
         System.out.println(context);
+    }
+
+    @After(event = CqnService.EVENT_READ, entity = ItemConfigs_.CDS_NAME)
+    void afterReadItem(CdsReadEventContext context) {
+        System.out.println("ItemServiceHandler.afterReadItem() called");
+        final var items = context.getResult().stream()
+                .filter(item -> Integer.parseInt(item.get("stock").toString()) > 0)
+                .peek(System.out::println)
+                .collect(Collectors.toList());
+
+        System.out.println(items);
+
     }
 }
